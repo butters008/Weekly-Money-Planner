bf2c61ae6095afccf09cdfb3a3f2aac23c8b134a
Butters <k.butterfield08@gmail.com>
2020-10-17 12:09:17 -0500

    Update index.php
    Turing computer off so making the commit of what I have here.

--- index.php
+++ index.php
@@ -4,6 +4,43 @@ echo "Hello!"
 ?>
 
 <table>
-
+    <thead>
+        <tr>
+            <th>Monday</th>
+            <th>Tuesday</th>
+            <th>Wednesday</th>
+            <th>Thursday</th>
+            <th>Friday</th>
+            <th>Saturday</th>
+            <th>Sunday</th>
+        </tr>
+        <!-- We are going to have the total amounts each day -->
+        <tr>
+            <th>Amount: </th>
+            <th>Amount: </th>
+            <th>Amount: </th>
+            <th>Amount: </th>
+            <th>Amount: </th>
+            <th>Amount: </th>
+            <th>Amount: </th>
+        </tr>
+    </thead>
+    <!-- This is where we are going to have the items Depost/Widthdrawls to be added-->
+    <tbody>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody>
+    <tfoot>
+        <tr>
+            <th></th>
+        </tr>
+    </tfoot>
 </table>
 <?php include "helperFiles/footer.php"; ?>
